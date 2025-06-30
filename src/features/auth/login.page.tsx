603250f954ad8a0fd25875a501ebd0938b00e62e
SukhanovIgorG <sukhanovgarik@yandex.ru>
2025-06-30 18:02:07 +0300

    feat: add shad-cn and settings components alias for them

--- src/features/auth/login.page.tsx
+++ src/features/auth/login.page.tsx
@@ -1,12 +1,17 @@
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 import { ROUTES } from '@/shared/model/routes';
+import { Button } from '@/shared/ui/kit/button';
 
 function LoginPage() {
+  const navigate = useNavigate();
   return (
     <div className="w-full h-screen flex justify-center items-center">
       <div className="border-1 rounded-2xl p-4 w-fit">
-        Нет аккаунта? <Link to={ROUTES.REGISTER}>Зарегистрироваться</Link>
+        Нет аккаунта?
+        <Button onClick={() => navigate(ROUTES.REGISTER)} variant={'link'}>
+          Зарегистрироваться
+        </Button>
       </div>
     </div>
   );
--- src/features/auth/register.page.tsx
+++ src/features/auth/register.page.tsx
@@ -1,12 +1,17 @@
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 import { ROUTES } from '@/shared/model/routes';
+import { Button } from '@/shared/ui/kit/button';
 
 function RegisterPage() {
+  const navigate = useNavigate();
   return (
     <div className="w-full h-screen flex justify-center items-center">
       <div className="border-1 rounded-2xl p-4 w-fit">
-        Уже есть аккаунт? <Link to={ROUTES.LOGIN}>Войти</Link>
+        Уже есть аккаунт?
+        <Button onClick={() => navigate(ROUTES.LOGIN)} variant={'link'}>
+          Войти
+        </Button>
       </div>
     </div>
   );
