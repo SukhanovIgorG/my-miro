86ce23f78697ee857ccd822d8be6a83007f695cf
SukhanovIgorG <sukhanovgarik@yandex.ru>
2025-07-02 11:00:44 +0300

    feat: обновить страницы входа и регистрации с использованием новых компонентов Card и Input

--- src/features/auth/login.page.tsx
+++ src/features/auth/login.page.tsx
@@ -1,18 +1,67 @@
-import { useNavigate } from 'react-router-dom';
-
 import { ROUTES } from '@/shared/model/routes';
-import { Button } from '@/shared/ui/kit/button';
+import {
+  Button,
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+  Input,
+  Label,
+} from '@/shared/ui/kit';
 
 function LoginPage() {
-  const navigate = useNavigate();
   return (
     <div className="w-full h-screen flex justify-center items-center">
-      <div className="border-1 rounded-2xl p-4 w-fit">
-        Нет аккаунта?
-        <Button onClick={() => navigate(ROUTES.REGISTER)} variant={'link'}>
-          Зарегистрироваться
-        </Button>
-      </div>
+      <Card className="w-full max-w-sm">
+        <CardHeader>
+          <CardTitle>Вход в аккаунт</CardTitle>
+          <CardDescription>Введите свои данные для входа.</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="flex flex-col gap-6">
+              <div className="grid gap-2">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  required
+                />
+              </div>
+              <div className="grid gap-2">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Пароль</Label>
+                  <a
+                    href="#"
+                    className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                  >
+                    Забыли пароль?
+                  </a>
+                </div>
+                <Input id="password" type="password" required />
+              </div>
+            </div>
+          </form>
+        </CardContent>
+        <CardFooter className="flex-col gap-2">
+          <Button type="submit" className="w-full">
+            Войти
+          </Button>
+          <Button variant="outline" className="w-full">
+            Войти через Google
+          </Button>
+          <CardDescription>
+            Если нет аккаунта, вы можете{' '}
+            <a href={ROUTES.REGISTER} className="underline-offset-4 underline">
+              зарегистрироваться
+            </a>
+            .
+          </CardDescription>
+        </CardFooter>
+      </Card>
     </div>
   );
 }
--- src/features/auth/register.page.tsx
+++ src/features/auth/register.page.tsx
@@ -1,18 +1,64 @@
-import { useNavigate } from 'react-router-dom';
-
 import { ROUTES } from '@/shared/model/routes';
-import { Button } from '@/shared/ui/kit/button';
+import {
+  Button,
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+  Input,
+  Label,
+} from '@/shared/ui/kit';
 
 function RegisterPage() {
-  const navigate = useNavigate();
   return (
     <div className="w-full h-screen flex justify-center items-center">
-      <div className="border-1 rounded-2xl p-4 w-fit">
-        Уже есть аккаунт?
-        <Button onClick={() => navigate(ROUTES.LOGIN)} variant={'link'}>
-          Войти
-        </Button>
-      </div>
+      <Card className="w-full max-w-sm">
+        <CardHeader>
+          <CardTitle>Регистрация</CardTitle>
+          <CardDescription>Создайте новый аккаунт.</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="flex flex-col gap-6">
+              <div className="grid gap-2">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  required
+                />
+              </div>
+              <div className="grid gap-2">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Пароль</Label>
+                </div>
+                <Input id="password" type="password" required />
+              </div>
+              <div className="grid gap-2">
+                <div className="flex items-center">
+                  <Label htmlFor="confirm">Повторите пароль</Label>
+                </div>
+                <Input id="confirm" type="password" required />
+              </div>
+            </div>
+          </form>
+        </CardContent>
+        <CardFooter className="flex-col gap-2">
+          <Button type="submit" className="w-full">
+            Зарегистрироваться
+          </Button>
+          <CardDescription>
+            Уже зарегистрированы?{' '}
+            <a href={ROUTES.LOGIN} className="underline-offset-4 underline">
+              Войдите в аккаунт
+            </a>
+            .
+          </CardDescription>
+        </CardFooter>
+      </Card>
     </div>
   );
 }
